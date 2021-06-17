a95f0ccb7f57ef52478b9fe4399812411d7005bf
Aditya Suresh Nagar <an7947@srmist.edu.in>
2021-06-17 22:38:32 +0530

    testing

--- src/components/card-files/card-body.js
+++ src/components/card-files/card-body.js
@@ -3,5 +3,8 @@ import './card.styles.css'
 import CardComponent from './card.component';
 export default (props)=>{  
     return(<div className='card-body'>
-    {props.monsters.map(monster => {return(<CardComponent key={monster.id} monster={monster} /> )})} 
+     <h1>"Aditya"</h1>
+    {props.monsters.map(monster => {return(
+        <CardComponent key={monster.id} monster={monster} /> 
+        )})} 
     </div>)};
